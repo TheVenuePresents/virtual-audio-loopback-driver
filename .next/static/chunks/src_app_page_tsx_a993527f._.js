7e028f5d9cbc7dac7b35c44e88fd4d41704153b6
TheVenue Curator <thevenuepresentshostbot@gmail.com>
2025-07-17 11:44:51 -0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1752777891987

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_67ba675f._.js",
+  "static/chunks/node_modules_cf3d513a._.js"
+],
+    source: "dynamic"
+});
